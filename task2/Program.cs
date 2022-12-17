24bdd132c31b43bff641bbf9c2e4cd2c2b5184ec
Anastasiya <anastasiyaosnov.mail.ru>
2022-12-17 09:58:20 +0300

    t

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. Не использовать строки для расчета
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+
+bool ValidateNumber(int number)
+{
+if (number >= 100 && number < 1000)
+{
+return true;
+}
+
+System.Console.WriteLine("Число не трехзначное");
+return false;
+}
+
+if (ValidateNumber(number))
+{
+int result1 = number % 10;
+int result2 = number /100;
+System.Console.WriteLine($"Полученное число {result2}{result1}");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+// Не использовать строки для расчета.
+
+// int Prompt(string message)
+// {
+// System.Console.Write($"{message} >");
+// int answer = Convert.ToInt32(Console.ReadLine());
+// return answer;
+// }
+
+// bool ValidateNumber(int number)
+// {
+// if (number >= 100)
+// {
+// return true;
+// }
+
+// System.Console.WriteLine("Третьей цифры нет");
+// return false;
+// }
+
+
+// int number = Prompt ("Введите число");
+// int result = number / 100;
+// Console.WriteLine(result);
+
+Console.WriteLine(103%10);
