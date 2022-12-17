23096a73074eac9b935e152cc07baadf65566ed9
Anastasiya <anastasiyaosnov.mail.ru>
2022-12-17 10:47:41 +0300

    task4

--- task3/Program.cs
+++ task3/Program.cs
@@ -4,29 +4,28 @@
 
 int Prompt(string message)
 {
-System.Console.Write($"{message} >");
-int uservalue = Convert.ToInt32(Console.ReadLine());
-return uservalue;
+    System.Console.Write($"{message} >");
+    int uservalue = Convert.ToInt32(Console.ReadLine());
+    return uservalue;
 }
 
-int number = Prompt ("Введите число");
+int number = Prompt("Введите число");
 Console.WriteLine(number);
-
 
 int value = number;
 
 if (number >= 100)
 
 {
-   while (value >= 999)
+    while (value >= 999)
     {
-       value = value / 10;
+        value = value / 10;
     }
-    Console.WriteLine ($"Третья цифра заданного числа = {value %10}");
+    Console.WriteLine($"Третья цифра заданного числа = {value % 10}");
 }
-else 
+else
 {
-Console.WriteLine("Нет третьего числа");
+    Console.WriteLine("Нет третьего числа");
 }
 
 
