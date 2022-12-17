7aac39314e4d52b4c9e53abea14449fb72e71354
Anastasiya <anastasiyaosnov.mail.ru>
2022-12-17 10:58:09 +0300

    all tasks

---
+++ task4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int uservalue = Convert.ToInt32(Console.ReadLine());
+    return uservalue;
+}
+
+int number = Prompt("Введите число");
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine(number + "-> Нет");
+        break;
+    case 2:
+        Console.WriteLine(number + "-> Нет");
+        break;
+    case 3:
+        Console.WriteLine(number + "-> Нет");
+        break;
+    case 4:
+        Console.WriteLine(number + "-> Нет");
+        break;
+    case 5:
+        Console.WriteLine(number + "-> Нет");
+        break;
+    case 6:
+        Console.WriteLine(number + "-> Да");
+        break;
+    case 7:
+        Console.WriteLine(number + "-> Да");
+        break;
+    default:
+    Console.WriteLine("Такого номера дня недели не существует, введите число от 1 до 7");
+    break;
+    
+}
